41579640987fb95d92b4f7bde9a5d77d8f248b49
Johnnyua <ev.dyachenko@gmail.com>
2023-06-08 13:32:14 +0300

    remove check user function

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -108,11 +108,8 @@ export default {
             user.phone = phone;
             user.position_id = this.positions.positionsList.find(item => { return item.name === user.position }).id;
 
-            // --------------- Check form and check user exist
+            // --------------- Check form
             if (this.checkForm(user)) {
-                return;
-            };
-            if (this.checkUserExist(user)) {
                 return;
             };
 
@@ -143,7 +140,7 @@ export default {
             this.successReg = true;
             setTimeout((scope) => {
                 scope.successReg = false;
-            }, 3000, this);
+            }, 1000, this);
         },
         checkForm(user) {
             const emailPattern = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/gm;
@@ -179,11 +176,6 @@ export default {
                 }
             }
             return hasError;
-        },
-        checkUserExist(user) {
-            this.loadUsers(true);
-            // console.log(this.users);
-            return false;
         },
         getImageDimensions(file) {
             let img = {
